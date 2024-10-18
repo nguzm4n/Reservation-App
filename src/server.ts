837222e125a1da8caaf5bdbef567c32d24a06417
Nicolás Guzmán <n3kguzman@gmail.com>
2024-10-18 16:23:29 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import { env } from "process"
+
+const app = express()
+
+const PORT : number = Number(env.PORT) || 1234
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('<h1> Hello World <h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`);
+})
