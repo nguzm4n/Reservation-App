8b7e2ef2a146cdc1d5ee78190403d090612eae68
Nicolás Guzmán <n3kguzman@gmail.com>
2024-10-21 21:56:20 -0300

    test2

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).send('<h1> Hello World <h1>');
 });
 
+
 app.listen(PORT, () => {
     console.log(`server listening on port http://localhost:${PORT}`);
 })
